2c923074ffbfb4997ec80a6bd1114fde86d48b7e
takashiono2 <takashiono710@gmail.com>
2021-07-10 13:44:12 +0900

    カレンダーにバインディングする

--- js/main.js
+++ js/main.js
@@ -1,22 +1,64 @@
-var app = new Vue({
+var app = new Vue({//arrival_date、//result、//slice(-2),function(dt),var m,var d
   el: '#app',
   data:{
-    width: window.innerWidth,
-    height: window.innerHeight,
+    arrival_date: null
   },
-  created: function(){
-    addEventListener('resize',this.resizeHandler);//（イベントのtype,通知を受け取るオブジェクトlistener）
+  created: function(){//初期値を設定する
+    this.arrival_date = this.formatDate(new Date());
   },
-  beforeDestroy: function(){
-    removeEventListener('resize',this.resizeHandler);
-  },
-  methods:{
-    resizeHandler: function($event){//$eventはイベントを受け取るイベント変数名
-      this.width = $event.target.innerWidth;//e.target はイベントを発生させる原因となった要素をとってくる
-      this.height = $event.target.innerHeight;//ターゲットにはwindowオブジェクトが代入される。
-    }
+  methods: {
+    formatDate: function(dt){//日付をYYYY/MM/DDに変換
+      var y = dt.getFullYear();
+      var m = ('00'+(dt.getMonth()+1)).slice(-2);
+      var d = ('00'+dt.getDate()).slice(-2);
+      var result = y+"-"+m+"-"+d ;
+    return result
   }
+}
 });
+
+// No.3 イベントハンドリング-resizeのイベントハンドラ2
+// var app = new Vue({ //mousemove,mousemoveHandler,clientX,clientY
+//   el: '#app',
+//   data: {
+//     point:{
+//       x: 0,
+//       y: 0
+//     }
+//   },
+//   created: function(){
+//     addEventListener('mousemove',this.mousemoveHandler);
+//   },
+//   beforeDestroy: function(){
+//     removeEventListener('mousemove',this.mousemoveHandler);
+//   },
+//   methods: {
+//     mousemoveHandler: function($event){
+//       this.point.x  = $event.clientX;
+//       this.point.y = $event.clientY;
+//     }
+//   }
+// });
+// No.3 イベントハンドリング-resizeのイベントハンドラ-
+// var app = new Vue({
+//   el: '#app',
+//   data:{
+//     width: window.innerWidth,
+//     height: window.innerHeight,
+//   },
+//   created: function(){
+//     addEventListener('resize',this.resizeHandler);//（イベントのtype,通知を受け取るオブジェクトlistener）
+//   },
+//   beforeDestroy: function(){
+//     removeEventListener('resize',this.resizeHandler);
+//   },
+//   methods:{
+//     resizeHandler: function($event){//$eventはイベントを受け取るイベント変数名
+//       this.width = $event.target.innerWidth;//e.target はイベントを発生させる原因となった要素をとってくる
+//       this.height = $event.target.innerHeight;//ターゲットにはwindowオブジェクトが代入される。
+//     }
+//   }
+// });
 
 // No.3 イベントハンドリング-算出プロパティ-
 // var app = document.querySelector('#app');
